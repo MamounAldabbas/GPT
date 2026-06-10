29eac55efd0197d92a75f02a581d81061e3b8f10
Mamoun Aldabbas <mdbbs32@gmail.com>
2026-06-10 15:01:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Website loaded.");
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", e => {
+cursor.style.left = e.clientX + "px";
+cursor.style.top = e.clientY + "px";
+});
+
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+});
+
+document.querySelectorAll(".hidden").forEach(el=>{
+observer.observe(el);
+});
+
+document.querySelector("form").addEventListener("submit",(e)=>{
+e.preventDefault();
+alert("Inquiry submitted.");
 });
